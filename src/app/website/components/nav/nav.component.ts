bc2956c877a30b3a3c5fe3f3903b856e0c481f0a
AagundisR <agundisRikolino@gmail.com>
2022-10-19 19:22:57 -0500

    [Update] layout

--- src/app/website/components/nav/nav.component.ts
+++ src/app/website/components/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  constructor(private elem: ElementRef) { }
 
   ngOnInit(): void {
   }
 
+  //Elementos para el dropdown---------------------------------------------------------
+  options: Array<String> = ["Casa", "Deportes", "Electrónicos"];
+  
+  public currentValue:any;
+  public dropdownOpen: boolean = false;
+  public get dropdownElement(): Element {return this.elem.nativeElement.querySelector('.dropdown-list')}
+  private currentIndex = -1;
+
+  closeDropdown() {
+    this.dropdownElement.setAttribute('aria-expanded', "false");
+    this.currentIndex = -1;
+    this.dropdownOpen = false;
+  }
+
+  selectByIndex(i: number) {
+    let value = this.options[i];
+    this.select(value);
+  }
+
+  select(value:any) {
+    this.currentValue = value;
+    this.closeDropdown();
+  }
+
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen;
+    this.dropdownElement.setAttribute('aria-expanded', this.dropdownOpen ? "true" : "false");
+  }
 }
