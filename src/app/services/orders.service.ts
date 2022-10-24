6e271291b02a810d528e8927b0503a190c61484f
whomafp <wickeyword@hotmail.com>
2022-10-24 18:37:14 -0500

    [UPDATE] Add order func to get service data.

--- src/app/services/orders.service.ts
+++ src/app/services/orders.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { Order } from './../models/order.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrdersService {
+  
+  apiUrl = 'http://ecommerceapi.x10.mx/api';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  setOrderAsCompleted(id: number) {
+    //here we must give a token
+    return this.http.post<Order>(`${this.apiUrl}/orders/${id}`);
+   }
+  
+   createOrder(data: Partial<Order>) { 
+    return this.http.post<Order>(`${this.apiUrl}/orders/`, data);
+   }
 }
