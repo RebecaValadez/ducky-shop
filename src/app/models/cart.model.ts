581a86bbaed50cae82c2bb960a93890d5c68d1a9
TorresGlz <50779817+TorresGlz@users.noreply.github.com>
2022-10-24 17:57:15 -0500

    cart model

--- src/app/models/cart.model.ts
+++ src/app/models/cart.model.ts
@@ -1,7 +1,5 @@
-import { Product } from './../models/product.model';
-
 export interface Cart {
   product_id: number;
   quantity: number;
-  // product_list: ;  falta definir qué tipo es
+  product_list: Array<number|number>;
 }
