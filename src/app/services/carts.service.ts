cff86e55ec054023f274a249ca89388747f0e47d
TorresGlz <50779817+TorresGlz@users.noreply.github.com>
2022-10-24 17:56:41 -0500

    cart service

--- src/app/services/carts.service.ts
+++ src/app/services/carts.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/common/http';
+import { Cart } from '../models/cart.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartsService {
 
-  constructor() { }
+  apiUrl = '';//Aqui va la url de la api
+
+  constructor(private http: HttpClient) { }
+
+  insertToCart(data: Partial<Cart>){
+   return this.http.post(`${this.apiUrl}/carts`, data);
+  }
+
+  deleteItemFromCart(product_id:number){
+    return this.http.delete(`${this.apiUrl}/carts/${product_id}`);
+  }
+
+  getCart(){
+    return this.http.get<Cart[]>(`${this.apiUrl}/carts`);
+  }
+
+  //deleteAllFromCart(){}
+
 }
