faae91d0a192cfe68a19fb86e91e24c6c93fdc90
AagundisR <agundisRikolino@gmail.com>
2022-10-26 19:19:14 -0500

    [Update] product-detail page

--- src/app/website/pages/product-detail/product-detail.component.ts
+++ src/app/website/pages/product-detail/product-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-product-detail',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-detail.component.scss']
 })
 export class ProductDetailComponent implements OnInit {
+  producto!: Product;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.producto = {
+      id: 1,
+      name: "Laptop",
+      price: 999999,
+      description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde vel totam at ad sint adipisci qui porro optio, deleniti blanditiis voluptas ea, dolore repudiandae. Excepturi aliquid architecto quo aspernatur saepe!",
+      category_id: 1,
+      category_name: "Tecnologia",
+      stock: 999999,
+      created_at: new Date(),
+      updated_at: new Date()
+    }
   }
 
 }
