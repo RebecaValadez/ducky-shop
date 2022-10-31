bdf69c3397b0ffae47a7b50e7a1481567a97dcb0
AagundisR <agundisRikolino@gmail.com>
2022-10-31 14:06:04 -0600

    [Update] login

--- src/app/website/pages/login/login.component.ts
+++ src/app/website/pages/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private render: Renderer2, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  addcl(e: any) {
+    this.render.addClass(e?.target, "focus");
+  }
+
+  remcl(e: any) {
+    this.render.removeClass(e?.target, "focus");
+  }
+
+  redirect() {
+    this.router.navigate(["/register"]);
+  }
 }
