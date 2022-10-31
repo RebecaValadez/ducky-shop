773af90c2c95ae82564d4ae43085e9d47706b7f4
AagundisR <agundisRikolino@gmail.com>
2022-10-31 14:50:15 -0600

    [Update] home

--- src/app/website/pages/home/home.component.ts
+++ src/app/website/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  productos: Array<Product> = [];
   constructor() { }
 
   ngOnInit(): void {
+    this.productos.push({
+      id: 1,
+      code: "string",
+      name: "Producto 1",
+      price: 999999,
+      description: "Descripcion",
+      category_id: 1,
+      category_name: "Alimentos",
+      stock: 999999,
+      created_at: new Date(),
+      updated_at: new Date()
+    });
   }
 
 }
