41d74b1e9fb56165e53968386fd47f495c8d0815
TorresGlz <50779817+TorresGlz@users.noreply.github.com>
2022-11-13 19:31:07 -0600

    [Cart] service update

--- src/app/services/carts.service.ts
+++ src/app/services/carts.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
-import { Injectable } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Cart } from '../models/cart.model';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartsService {
 
-  apiUrl = '';//Aqui va la url de la api
+  apiUrl = 'http://localhost:8000/api';
 
   constructor(private http: HttpClient) { }
 
@@ -23,6 +24,8 @@ export class CartsService {
     return this.http.get<Cart[]>(`${this.apiUrl}/carts`);
   }
 
-  //deleteAllFromCart(){}
-
+/*  deleteAllFromCart(product_id:number){
+    return this.http.delete(`${this.apiUrl}/carts` + product_id).forEach
+  }
+*/
 }
