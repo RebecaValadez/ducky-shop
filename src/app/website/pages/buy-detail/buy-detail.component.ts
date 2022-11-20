195dbf840a7ea644e222964d4654c6471c805477
AagundisR <agundisRikolino@gmail.com>
2022-11-20 00:32:37 -0600

    [Update] buy detail, order detail

--- src/app/website/pages/buy-detail/buy-detail.component.ts
+++ src/app/website/pages/buy-detail/buy-detail.component.ts
@@ -7,6 +7,7 @@ import { User } from 'src/app/models/user.model';
 import { UsersService } from 'src/app/services/users.service';
 import { Card } from 'src/app/models/card.model';
 import { CardsService } from 'src/app/services/cards.service';
+import { Payment } from 'src/app/models/payment.model';
 import { Router } from '@angular/router';
 
 @Component({
@@ -19,7 +20,7 @@ export class BuyDetailComponent implements OnInit {
   user_id!: number;
   cards: Card[] = [];
   products: Cart[] = [];
-  total : number = 0;
+  total: number = 0;
   order: Order = {
     id: 0,
     order_number: 2,
@@ -33,7 +34,7 @@ export class BuyDetailComponent implements OnInit {
     private usersService: UsersService,
     private cartsService: CartsService,
     private cardsService: CardsService,
-    private router:Router
+    private router: Router
   ) { }
 
   ngOnInit(): void {
@@ -47,49 +48,61 @@ export class BuyDetailComponent implements OnInit {
         this.user_id = this.user.id
         this.order.user_id = this.user_id
         this.usersService.getUser(this.user.id)
-        .subscribe(data => {
-          this.user = data
-          console.log(this.user)
-        })
+          .subscribe(data => {
+            this.user = data
+            console.log(this.user)
+          })
         this.getCard()
         this.getCartUser()
       })
   }
 
-  getCard(){
+  getCard() {
     this.cardsService.getCard(this.user_id)
-    .subscribe(data => {
-      this.cards = data.data
-      console.log(this.cards[0])
-    })
+      .subscribe(data => {
+        this.cards = data.data
+        console.log(this.cards[0])
+      })
   }
 
-  getCartUser(){
+  getCartUser() {
     this.cartsService.getUserCart(this.user_id)
-    .subscribe(data => {
-      this.products = data.data
-      this.totalBuy()
-    })
+      .subscribe(data => {
+        this.products = data.data
+        this.totalBuy()
+      })
   }
 
-  totalBuy(){
+  totalBuy() {
     this.products.forEach(product => {
       this.total = this.total + product.amount
     });
   }
 
-  createOrder(){
-    // console.log("Comienza")
-    // this.products.forEach(cart => {
-    //   //El número de orden debe ser el mismo para los carritos
-    //   this.order.order_number = 2
-    //   this.order.cart_id = cart.id
-    //   console.log(this.order)
-    //   this.ordersService.createOrder(this.order)
-    //   .subscribe(() =>{
-    //     this.cartsService.deleteProductOnCart(cart.id)
-    //   })
-    // });
-    this.router.navigate(['/order-detail'])
+  createOrder() {
+    var order_number: number = Date.now()
+    this.order.order_number = order_number
+    this.order.user_id = this.user.id
+    var ord!: Order[];
+    this.products.forEach(cart => {
+      //El número de orden debe ser el mismo para los carritos
+      this.order.cart_id = cart.id
+
+      var aux: Order = this.order
+      ord.push(aux)
+
+      this.ordersService.createOrder(this.order).subscribe(() => { })
+    });
+
+    var pay: Payment = {
+      order_number: order_number,
+      card_id: this.cards[0].id,
+      cards: this.cards[0].id,
+      amount: this.total
+    }
+
+    localStorage.setItem("order", JSON.stringify(ord))
+
+    this.router.navigate(['/order-detail'], { queryParams: { order: order_number } })
   }
 }
--- src/app/website/pages/order-detail/order-detail.component.ts
+++ src/app/website/pages/order-detail/order-detail.component.ts
@@ -3,7 +3,11 @@ import { Order } from 'src/app/models/order.model';
 import { OrdersService } from 'src/app/services/orders.service';
 import { User } from 'src/app/models/user.model';
 import { UsersService } from 'src/app/services/users.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CartsService } from 'src/app/services/carts.service';
+import { Cart } from 'src/app/models/cart.model';
+import { Product } from 'src/app/models/product.model';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-order-detail',
@@ -14,19 +18,39 @@ export class OrderDetailComponent implements OnInit {
 
   user!: User;
   user_id!: number;
-  products: any[] = [];
-  total : number = 0;
+  carts: Cart[] = [];
+  total: number = 0;
   order!: Order
+  orders!: Order[];
   order_number: number = 0
 
   constructor(
     private ordersService: OrdersService,
     private usersService: UsersService,
-    private router:Router
+    private cartService: CartsService,
+    private router: Router,
+    private actRouter: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
     this.getUser()
+
+    this.actRouter.queryParams.subscribe(params => {
+      this.ordersService.getOrder(params["order"]).subscribe(data => {
+        this.orders = data.data["1"]
+        this.order = this.orders[0]
+
+        this.orders.forEach(order => {
+          this.cartService.getUserCart(this.user_id).subscribe(data => {
+            this.carts = data.data;
+
+            this.carts.forEach(cart => {
+              this.total += cart.amount
+            })
+          })
+        })
+      })
+    })
   }
 
   getUser() {
@@ -36,14 +60,14 @@ export class OrderDetailComponent implements OnInit {
         this.user_id = this.user.id
         this.order.user_id = this.user_id
         this.usersService.getUser(this.user.id)
-        .subscribe(data => {
-          this.user = data
-          console.log(this.user)
-        })
+          .subscribe(data => {
+            this.user = data
+            console.log(this.user)
+          })
       })
   }
 
-  backHome(){
+  backHome() {
     this.router.navigate(['/home'])
   }
 
